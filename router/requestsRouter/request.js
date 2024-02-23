d0036fd15df812bf80d668d7a9aeff07ae9252bc
ahmd bdarnh <ahmds3b@gmail.com>
2024-02-23 16:48:31 +0200

    fix multer error

--- router/requestsRouter/request.js
+++ router/requestsRouter/request.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const reqController = require('../../controllers/requsetController/requestcontrller');
-const upload = require('../../middleware/upload'); 
+const uploadMiddleware = require('../../middleware/upload'); // Renamed to uploadMiddleware to avoid conflict
 
-//request
+// request routes
 router.get("/request/:id", reqController.getReqByID);
 router.get("/request", reqController.getAllReq);
 router.post("/request", reqController.request_post);
 router.post("/request/update", reqController.req_update);
 router.post("/request/delete", reqController.req_delete);
 
-
-
+// home routes
 router.get("/home/request", reqController.getReqPage);
-router.post("/home/request", upload.single('image'), reqController.request_post); // Add uploadImage controller method
+router.post("/home/request", uploadMiddleware.single('image'), reqController.request_post); // Use uploadMiddleware instead of upload
 
 module.exports = router;
